38fe8222b170af94714ccd98bbd4103ef07daa12
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-02-17 14:15:27 +0900

    Add aizu_online_judge/ITP1_4_D/4180294.cpp

---
+++ aizu_online_judge/ITP1_4_D/4180294.cpp
@@ -0,0 +1,79 @@
+// URL: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=4180294
+// Date: Mon, 17 Feb 2020 14:15:27 +0900
+// Language: C++14
+// header {{{
+using namespace std;
+
+// C++
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <streambuf>
+#include <string>
+#include <utility>
+#include <vector>
+
+#pragma GCC optimization_level 3
+
+// util {{{
+#define print(x)	        cout << (x)
+#define printvec(x)       for(int i = 0;i < (x).size();i++)cout << (x).at(i) << " ";cout << endl
+#define println(x)	        cout << (x) << endl
+#define printn		        cout << endl
+#define read(x)	            cin >> (x)
+#define readvec(x)        for(int i = 0; i < (x).size();i++)cin >> (x).at(i)
+
+#define rep(i, n)	        for(int i = 0; i < (int)(n); i++)
+#define rep_1(i, n) 	    for(int i = 1; i <=(int)(n); i++)
+
+#define all(x) 		        (x).begin(),(x).end()
+#define vint                vector<int>
+#define outif(x,y,z)        if(x) cout<<(y)<<endl; else cout<<(z)<<endl
+#define loop 		        while(true)
+
+// }}}
+
+// init {{{
+struct ProconInit {
+    static constexpr int IOS_PREC = 15;
+    static constexpr bool AUTOFLUSH = false;
+
+    ProconInit() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(IOS_PREC);
+        if(AUTOFLUSH)
+            cout << unitbuf;
+    }
+} PROCON_INIT;
+// }}}
+
+//--------------------------------------------------------------------
+
+void solve() {
+    int n;
+    long long int min,max,sum = 0;
+    read(n);
+    vint vec(n);
+    read(min);
+    max = min;
+    sum += min;
+    rep(i,n - 1){
+        int tmp;
+        read(tmp);
+        if(tmp > max){
+            max = tmp;
+        } else if(tmp < min){
+            min = tmp;
+        }
+        sum += tmp;
+    }
+    cout << min << " " << max << " " << sum << endl;
+}
+
+signed main() {
+    solve();
+    return 0;
+}
