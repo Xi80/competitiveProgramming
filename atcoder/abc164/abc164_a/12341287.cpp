6ac57344aafbea81ffe414dc9d66172e626aaf5a
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-04-26 21:03:25 +0900

    Add atcoder/abc164/abc164_a/12341287.cpp

---
+++ atcoder/abc164/abc164_a/12341287.cpp
@@ -0,0 +1,14 @@
+// URL: https://atcoder.jp/contests/abc164/submissions/12341287
+// Date: Sun, 26 Apr 2020 21:03:25 +0900
+// Language: C++ (GCC 9.2.1)
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+	int a,b;
+  	cin >> a >> b;
+  	if(a > b){
+    	cout << "safe" << endl;
+    } else {
+    	cout << "unsafe" << endl;
+    }
+}
