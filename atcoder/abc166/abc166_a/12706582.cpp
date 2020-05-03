ed647e44677c5e59ea1e0e367b037e09679578b8
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-05-03 21:00:49 +0900

    Add atcoder/abc166/abc166_a/12706582.cpp

---
+++ atcoder/abc166/abc166_a/12706582.cpp
@@ -0,0 +1,15 @@
+// URL: https://atcoder.jp/contests/abc166/submissions/12706582
+// Date: Sun, 03 May 2020 21:00:49 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if(s == "ABC"){
+        cout << "ARC" << endl;
+    } else{
+        cout << "ABC" << endl;
+    }
+}
