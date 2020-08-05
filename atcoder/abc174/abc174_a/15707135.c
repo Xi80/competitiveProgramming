6d3337fe0ac979448603458c30d4a63b87e608c9
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-05 20:58:43 +0900

    Add atcoder/abc174/abc174_a/15707135.c

---
+++ atcoder/abc174/abc174_a/15707135.c
@@ -0,0 +1,16 @@
+// URL: https://atcoder.jp/contests/abc174/submissions/15707135
+// Date: Wed, 05 Aug 2020 20:58:43 +0900
+// Language: C (GCC 9.2.1)
+#include<stdio.h>
+
+
+int main(void){
+  int a = -41;
+  scanf("%d",&a);
+  if(a >= 30){
+    printf("Yes\n");
+  } else {
+    printf("No\n");
+  }
+  return 0;
+}
