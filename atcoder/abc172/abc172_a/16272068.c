5fa7d635ba46256738a04fc89dcadc986b80bb37
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-28 10:01:35 +0900

    Add atcoder/abc172/abc172_a/16272068.c

---
+++ atcoder/abc172/abc172_a/16272068.c
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc172/submissions/16272068
+// Date: Fri, 28 Aug 2020 10:01:35 +0900
+// Language: C (GCC 9.2.1)
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a * (1 + a + a * a));
+    return 0;
+}
