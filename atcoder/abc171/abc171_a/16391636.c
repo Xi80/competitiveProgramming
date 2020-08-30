914996b7fe5c48c1d6ca1e7921969cf522e1493d
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-30 11:47:25 +0900

    Add atcoder/abc171/abc171_a/16391636.c

---
+++ atcoder/abc171/abc171_a/16391636.c
@@ -0,0 +1,10 @@
+// URL: https://atcoder.jp/contests/abc171/submissions/16391636
+// Date: Sun, 30 Aug 2020 11:47:25 +0900
+// Language: C (GCC 9.2.1)
+#include <stdio.h>
+
+int main() {
+    char a = getchar();
+    putchar(('A' <= a && a <= 'Z')? 'A' : 'a');
+    return 0;
+}
