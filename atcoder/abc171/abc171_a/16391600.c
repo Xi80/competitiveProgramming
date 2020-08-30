319534d868d24750f44dbf0af0119592bf569d4e
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-30 11:45:08 +0900

    Add atcoder/abc171/abc171_a/16391600.c

---
+++ atcoder/abc171/abc171_a/16391600.c
@@ -0,0 +1,4 @@
+// URL: https://atcoder.jp/contests/abc171/submissions/16391600
+// Date: Sun, 30 Aug 2020 11:45:08 +0900
+// Language: C (GCC 9.2.1)
+a;main(){a=getchar();puts(('A'<=a&&a<='Z')?"A":"a");}
