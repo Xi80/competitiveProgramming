3e3e148d1b6596c83b5eda10bded09556cb16bde
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-08-30 12:51:11 +0900

    Add atcoder/abc169/abc169_a/16392947.c

---
+++ atcoder/abc169/abc169_a/16392947.c
@@ -0,0 +1,11 @@
+// URL: https://atcoder.jp/contests/abc169/submissions/16392947
+// Date: Sun, 30 Aug 2020 12:51:11 +0900
+// Language: C (GCC 9.2.1)
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a * b);
+    return 0;
+}
