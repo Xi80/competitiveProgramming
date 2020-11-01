9b7cd893545be4180b1b1398fa4d21d34ef78328
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-11-01 21:00:48 +0900

    Add atcoder/abc181/abc181_a/17780076.cpp

---
+++ atcoder/abc181/abc181_a/17780076.cpp
@@ -0,0 +1,18 @@
+// URL: https://atcoder.jp/contests/abc181/submissions/17780076
+// Date: Sun, 01 Nov 2020 21:00:48 +0900
+// Language: C++ (GCC 9.2.1)
+#include <bits/stdc++.h>
+#define rep(i, n) for (int (i) = 0; (i) < (int)(n); (i)++)
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n % 2){
+        cout << "Black" << endl;
+    } else {
+        cout << "White" << endl;
+    }
+    return EXIT_SUCCESS;
+}
